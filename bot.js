6c574128fb49d1f20a3c449cc8dd4e83fb63ed00
jjbb1221 <41380360+jjbb1221@users.noreply.github.com>
2018-07-29 22:37:46 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const client.Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message () => {
+    if (message.content === ';ping') {
+      message.reply('Pong!');
+      }
+});
+
+client.login(process.env.BOT_TOKEN);
